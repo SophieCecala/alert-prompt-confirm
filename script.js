14ad8c1904dca18f09c08e6fd513669f9107ae35
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:14:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*alert("hello!");
+
+let n = 300;
+alert(n);*/
+
+
+/*let name = prompt("what is your name", "liza");
+alert(`Hello ${name}`);*/
+
+let age =  prompt("How old are you?", "14")
+confirm(`are you ${age} yeaars old?`)
+
+alert(`your're ${age} years old`)
